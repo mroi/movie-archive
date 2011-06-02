a9551858c6293e9ebbb645bb5975120d546a7c62
Michael Roitzsch <reactorcontrol@icloud.com>
2011-06-02 16:58:58 +0000

    reorder methods to a more logical flow
    git-svn-id: https://svn.code.sf.net/p/dvd2ite/code/trunk@34 0fc63e2a-737a-4626-a4d0-5a3d12a8b6d1

--- dvd/DVDImportDocument.m
+++ dvd/DVDImportDocument.m
@@ -2,18 +2,31 @@
 
 #import "DVDImportDocument.h"
 
+
 @implementation DVDImportDocument
+
++ (BOOL)isURLSupported:(NSURL *)url
+{
+	NSString *devicePath = [[url filePathURL] path];
+	NSString *mediaPath = [devicePath stringByAppendingString:@"/VIDEO_TS"];
+	return [[[[NSFileManager alloc] init] autorelease] fileExistsAtPath:mediaPath];
+}
 
 - (id)init
 {
-    self = [super init];
-    if (self) {
-    
-        // Add your subclass-specific initialization here.
-        // If an error occurs here, send a [self release] message and return nil.
-    
+	if ((self = [super init])) {
+        // subclass-specific initialization goes here
     }
     return self;
+}
+
+- (id)initWithContentsOfURL:(NSURL *)url ofType:(NSString *)typeName error:(NSError **)outError
+{
+	if ((self = [self initWithType:typeName error:outError])) {
+		[self setFileURL:url];
+		// TODO: read the url and init the document
+	}
+	return self;
 }
 
 - (NSString *)windowNibName
@@ -25,19 +38,6 @@
 {
     [super windowControllerDidLoadNib:aController];
     // Add any code here that needs to be executed once the windowController has loaded the document's window.
-}
-
-+ (BOOL)isURLSupported:(NSURL *)url
-{
-	NSString *devicePath = [[url filePathURL] path];
-	NSString *mediaPath = [devicePath stringByAppendingString:@"/VIDEO_TS"];
-	return [[[[NSFileManager alloc] init] autorelease] fileExistsAtPath:mediaPath];
-}
-
-- (id)initWithContentsOfURL:(NSURL *)url ofType:(NSString *)typeName error:(NSError **)outError
-{
-	// TODO: read the url and create the document
-	return [self initWithType:typeName error:outError];
 }
 
 @end
