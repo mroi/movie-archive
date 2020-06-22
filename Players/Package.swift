7ddd35064511d9d88c1700eff14afc1eae2f6734
Michael Roitzsch <reactorcontrol@icloud.com>
2020-06-22 16:58:40 +0200

    add Swift package for Players subsystem

---
+++ Players/Package.swift
@@ -0,0 +1,15 @@
+// swift-tools-version:5.2
+import PackageDescription
+
+let package = Package(
+	name: "Players",
+	platforms: [
+		.macOS(.v10_15)
+	],
+	products: [
+//		.library(name: "Players", type: .static, targets: ["MovieArchivePlayers"]),
+	],
+	targets: [
+//		.target(name: "MovieArchivePlayers", path: ".")
+	]
+)
