1aadb89c1e426c75aa8310712aee3a8a13aee197
Michael Roitzsch <reactorcontrol@icloud.com>
2020-07-12 20:05:44 +0200

    adapt to SwiftUI life cycle management
    bumps deployment target to 10.16

--- macOS/ImportDocument.swift
+++ macOS/ImportDocument.swift
@@ -1,5 +1,5 @@
-import Cocoa
-
+import SwiftUI
+import UniformTypeIdentifiers
 import MovieArchiveImporters
 
 
@@ -8,41 +8,32 @@ import MovieArchiveImporters
 /// This document is called ‘input’ because this is what the user experiences in the context of the macOS app.
 /// However, internally it uses an importer and an exporter. The importer reads from the external source into
 /// the internal data model. The exporter outputs from the data model to the movie archive storage format.
-class ImportDocument: NSDocument {
+struct ImportDocument: FileDocument {
 
-	override init() {
-	    super.init()
-		// Add your subclass-specific initialization here.
+	static var readableContentTypes: [UTType] = [ .import ]
+
+	init() {}
+
+	init(fileWrapper: FileWrapper, contentType: UTType) throws {
+		guard let _ = fileWrapper.regularFileContents else {
+			throw CocoaError(.fileReadCorruptFile)
+		}
 	}
 
-	override class var autosavesInPlace: Bool {
-		return true
+	func write(to fileWrapper: inout FileWrapper, contentType: UTType) throws {
+		fileWrapper = FileWrapper(regularFileWithContents: Data())
 	}
+}
 
-	override func makeWindowControllers() {
-		// Create the window and set the content view.
-		let window = NSWindow(
-		    contentRect: NSRect(x: 0, y: 0, width: 480, height: 300),
-		    styleMask: [.titled, .closable, .miniaturizable, .resizable, .fullSizeContentView],
-		    backing: .buffered, defer: false)
-		window.center()
-		window.contentView = TestView.default
-		let windowController = NSWindowController(window: window)
-		self.addWindowController(windowController)
+extension UTType {
+	static var `import`: UTType {
+		UTType(importedAs: "de.reactorcontrol.movie-archive.import")
 	}
+}
 
-	override func data(ofType typeName: String) throws -> Data {
-		// Insert code here to write your document to data of the specified type, throwing an error in case of failure.
-		// Alternatively, you could remove this method and override fileWrapper(ofType:), write(to:ofType:), or write(to:ofType:for:originalContentsURL:) instead.
-		throw NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
+struct ImportDocumentView: View {
+	@Binding var document: ImportDocument
+	var body: some View {
+		TestView()
 	}
-
-	override func read(from data: Data, ofType typeName: String) throws {
-		// Insert code here to read your document from the given data of the specified type, throwing an error in case of failure.
-		// Alternatively, you could remove this method and override read(from:ofType:) instead.
-		// If you do, you should also override isEntireFileLoaded to return false if the contents are lazily loaded.
-		throw NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
-	}
-
-
 }
