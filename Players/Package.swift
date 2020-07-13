139807808f731a0cbd66d3b6e93c11804adcea06
Michael Roitzsch <reactorcontrol@icloud.com>
2020-07-13 09:05:33 +0200

    update Swift packages to latest tool versions

--- Players/Package.swift
+++ Players/Package.swift
@@ -1,10 +1,10 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
 	name: "Players",
 	platforms: [
-		.macOS(.v10_15)
+		.macOS(.v10_16)
 	],
 	products: [
 //		.library(name: "Players", type: .static, targets: ["MovieArchivePlayers"]),
