4db6f66ddd4b2e0598afb45f713ae9d43bd44826
Michael Roitzsch <reactorcontrol@icloud.com>
2021-07-19 15:20:59 +0200

    importer: find importer matching the source

--- Model/Pass.swift
+++ Model/Pass.swift
@@ -1,3 +1,6 @@
+import Foundation
+
+
 /// Operator to manipulate a `MediaTree`.
 ///
 /// A `Pass` takes a `MediaTree` as input and outputs a new one. It represents
@@ -6,11 +9,25 @@
 /// * A pass can invoke itself recursively on child trees.
 /// * A pass can invoke a chain of sub-passes on the tree.
 public protocol Pass {
+
+	/// The sub-passes invoked as part of the execution of this `Pass`.
+	var subPasses: [Pass] { get }
 }
 
 
 /// A special pass that receives no input.
-public protocol ImportPass: Pass {}
+public protocol ImportPass: Pass {
+
+	/// Creates an appropriate importer if the source is supported.
+	init(source url: URL) throws
+}
 
 /// A special pass that generates no output other than side effects.
 public protocol ExportPass: Pass {}
+
+
+/* MARK: Default Implementations */
+
+public extension Pass {
+	var subPasses: [Pass] { get { [] } }
+}
