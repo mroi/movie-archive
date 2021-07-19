mroi/dvd2ite

tests: expect success reading minimal DVD

Tests/Importers.swift
@@ -79,4 +79,9 @@ class DVDImporterTests: XCTestCase {
 		subscription.cancel()
 		connection.invalidate()
 	}
+
+	func testMinimalDVD() {
+		let iso = testBundle.url(forResource: "MinimalDVD", withExtension: "iso")!
+		XCTAssertNoThrow(try DVDImporter(fromSource: iso))
+	}
 }

mroi/movie-archive

Tests/Importers.swift
@@ -78,4 +78,9 @@ class DVDImporterTests: XCTestCase {
 
 		waitForExpectations(timeout: .infinity)
 	}
+
+	func testMinimalDVD() {
+		let iso = testBundle.url(forResource: "MinimalDVD", withExtension: "iso")!
+		XCTAssertNoThrow(try Importer(source: iso))
+	}
 }