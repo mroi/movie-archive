730eadfbb5b2d34ee0e56bf07cd465db0cde4542
Michael Roitzsch <reactorcontrol@icloud.com>
2021-07-26 20:51:01 +0200

    importer: create opaque node from DVD info

--- Importers/DVDImporter.swift
+++ Importers/DVDImporter.swift
@@ -5,12 +5,18 @@ import MovieArchiveConverter
 
 struct DVDImporter: ImportPass {
 
+	private let dvdReader: DVDReader
+
 	init(source url: URL) throws {
-		let _ = try DVDReader(source: url)
+		dvdReader = try DVDReader(source: url)
 	}
 
 	func generate() throws -> MediaTree {
-		// FIXME: read DVD IFO data
-		return .opaque
+		// TODO: connect DVDReader publisher to transform subject
+
+		let info = try dvdReader.info()
+
+		let node = MediaTree.OpaqueNode(children: [], payload: info)
+		return .opaque(node)
 	}
 }
