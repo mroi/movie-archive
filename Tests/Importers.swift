d67016e0d6ea5dd0906134bc18b9d16a60d420b2
Michael Roitzsch <reactorcontrol@icloud.com>
2021-07-30 23:51:02 +0200

    tests: add test class for importers

---
+++ Tests/Importers.swift
@@ -0,0 +1,16 @@
+import XCTest
+
+@testable import MovieArchiveImporters
+@testable import MovieArchiveConverter
+
+
+/* MARK: Generic Importer Tests */
+
+class ImporterTests: XCTestCase {
+}
+
+
+/* MARK: DVD Importer Tests */
+
+class DVDImporterTests: XCTestCase {
+}
