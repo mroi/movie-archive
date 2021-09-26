4d14ea1c11c09565e98670fcf8fa33392afbaa52
Michael Roitzsch <reactorcontrol@icloud.com>
2021-09-26 17:41:24 +0200

    converter: DVD global info

--- XPCConverter/Converter/ResultDVDReader.swift
+++ XPCConverter/Converter/ResultDVDReader.swift
@@ -3,5 +3,38 @@
 /// Static information about the navigational and playback structure of the DVD.
 public struct DVDInfo: Codable {
 
-	public init() {}
+	public let specification: Version
+	public let category: UInt32
+
+	public let provider: String
+	public let posCode: UInt64
+	public let totalVolumeCount: UInt16
+	public let volumeIndex: UInt16
+	public let discSide: UInt8
+
+	public init(specification: Version,
+	            category: UInt32,
+	            provider: String,
+	            posCode: UInt64,
+	            totalVolumeCount: UInt16,
+	            volumeIndex: UInt16,
+	            discSide: UInt8) {
+		self.specification = specification
+		self.category = category
+		self.provider = provider
+		self.posCode = posCode
+		self.totalVolumeCount = totalVolumeCount
+		self.volumeIndex = volumeIndex
+		self.discSide = discSide
+	}
+
+	public struct Version: Codable {
+		public let major: UInt8
+		public let minor: UInt8
+
+		public init(major: UInt8, minor: UInt8) {
+			self.major = major
+			self.minor = minor
+		}
+	}
 }
--- XPCConverter/DVDInfoInit.swift
+++ XPCConverter/DVDInfoInit.swift
@@ -6,7 +6,20 @@ import LibDVDRead
 
 extension DVDInfo {
 	init?(_ ifoData: DVDData.IFO.All) {
-		// FIXME: transfer IFO information to DVDInfo initializer
-		self.init()
+		guard let vmgi = ifoData[.vmgi]?.pointee else { return nil }
+		guard let vmgiMat = vmgi.vmgi_mat?.pointee else { return nil }
+		self.init(specification: Version(vmgiMat.specification_version),
+		          category: vmgiMat.vmg_category,
+		          provider: String(tuple: vmgiMat.provider_identifier),
+		          posCode: vmgiMat.vmg_pos_code,
+		          totalVolumeCount: vmgiMat.vmg_nr_of_volumes,
+		          volumeIndex: vmgiMat.vmg_this_volume_nr,
+		          discSide: vmgiMat.disc_side)
+	}
+}
+
+private extension DVDInfo.Version {
+	init(_ combined: UInt8) {
+		self.init(major: combined.bits(4...7), minor: combined.bits(0...3))
 	}
 }
