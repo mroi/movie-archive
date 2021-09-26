89ff31587e3d1b7780551a03ff7aaac9a5d83b58
Michael Roitzsch <reactorcontrol@icloud.com>
2021-09-26 18:03:17 +0200

    converter: determine program chains for NAV scan

--- XPCConverter/DVDReader.swift
+++ XPCConverter/DVDReader.swift
@@ -249,8 +249,26 @@ private extension DVDData.NAV {
 		}
 
 		func scan(_ file: DVDData.FileId, within ifoData: DVDData.IFO.All) throws {
-			// FIXME: scan for NAV packets and collect information
+			let vtsData = ifoData.vtsNavCells(for: file)
+
 			// FIXME: update progress with a fractional value
+
+			let vtsNav = vtsData.map { (domain, domainData) -> (DVDData.DomainId, Domain) in
+				guard domainData.contains(where: { $0.value.count > 0 }) else {
+					return (domain, [:])
+				}
+
+				let domainNav = domainData.mapValues { pgc -> PGC in
+					return pgc.map { cell -> Cell in
+
+						// FIXME: read NAV packets from cells
+						return []
+					}
+				}
+				return (domain, domainNav)
+			}
+
+			data[file] = VTS(uniqueKeysWithValues: vtsNav)
 		}
 	}
 }
@@ -262,6 +280,79 @@ private extension Dictionary where Key == DVDData.IFO.All.Key, Value == DVDData.
 	/// The number of Video Title Sets on the DVD.
 	var vtsCount: Int {
 		Int(self[.vmgi]?.pointee.vmgi_mat?.pointee.vmg_nr_of_title_sets ?? 0)
+	}
+
+	/// Information about all titles on the DVD.
+	var titleInfo: UnsafeBufferPointer<title_info_t> {
+		let titleTable = self[.vmgi]?.pointee.tt_srpt?.pointee
+		let titleStart = titleTable?.title
+		let titleCount = titleTable?.nr_of_srpts
+		return UnsafeBufferPointer(start: titleStart, count: titleCount)
+	}
+
+	/// Cells within program chains within a title set domain that should be scanned for NAV packets.
+	func vtsNavCells(for key: Key) -> [DVDData.DomainId: [DVDData.PGCId: [cell_playback_t]]] {
+		func firstPlayProgramChain() -> [DVDData.PGCId: [cell_playback_t]] {
+			self[.vmgi]?.pointee.first_play_pgc.map { [.firstPlay: cells(within: $0.pointee)] } ?? [:]
+		}
+
+		func menuProgramChains(forKey key: Key) -> [DVDData.PGCId: [cell_playback_t]] {
+			let languagesTable = self[key]?.pointee.pgci_ut?.pointee
+			let languagesStart = languagesTable?.lu
+			let languagesCount = languagesTable?.nr_of_lus
+			let languages = UnsafeBufferPointer(start: languagesStart, count: languagesCount)
+			let pgcs = languages.flatMap { language -> [(DVDData.PGCId, [cell_playback_t])] in
+				let pgcInfosTable = language.pgcit?.pointee
+				let pgcInfosStart = pgcInfosTable?.pgci_srp
+				let pgcInfosCount = pgcInfosTable?.nr_of_pgci_srp
+				let pgcInfos = UnsafeBufferPointer(start: pgcInfosStart, count: pgcInfosCount)
+				return pgcInfos.compactMap { pgcInfo in
+					let id = DVDData.PGCId.menu(language: language.lang_start_byte,
+					                            pgc: pgcInfo.pgc_start_byte)
+					return pgcInfo.pgc.map { (id, cells(within: $0.pointee)) } ?? nil
+				}
+			}
+			return .init(uniqueKeysWithValues: pgcs)
+		}
+
+		func titleProgramChains(forIndex index: Int) -> [DVDData.PGCId: [cell_playback_t]] {
+			let pgcInfosTable = self[.vtsi(index)]?.pointee.vts_pgcit?.pointee
+			let pgcInfosStart = pgcInfosTable?.pgci_srp
+			let pgcInfosCount = pgcInfosTable?.nr_of_pgci_srp
+			let pgcInfos = UnsafeBufferPointer(start: pgcInfosStart, count: pgcInfosCount)
+			let pgcs = pgcInfos.compactMap { pgcInfo -> (DVDData.PGCId, [cell_playback_t])? in
+				let interactive = titleInfo.contains {
+					// only include titles that contain interactive button commands
+					$0.title_set_nr == index &&
+					$0.vts_ttn == pgcInfo.entry_id.bits(0...6) &&
+					$0.pb_ty.jlc_exists_in_button_cmd != 0
+				}
+				guard interactive else { return nil }
+				let id = DVDData.PGCId.title(pgc: pgcInfo.pgc_start_byte)
+				return pgcInfo.pgc.map { (id, cells(within: $0.pointee)) } ?? nil
+			}
+			return .init(uniqueKeysWithValues: pgcs)
+		}
+
+		func cells(within pgc: pgc_t) -> [cell_playback_t] {
+			let cellsStart = pgc.cell_playback
+			let cellsCount = pgc.nr_of_cells
+			let cells = UnsafeBufferPointer(start: cellsStart, count: cellsCount)
+			return Array(cells)
+		}
+
+		switch key {
+		case .vmgi:
+			return [
+				.firstPlay: firstPlayProgramChain(),
+				.menus: menuProgramChains(forKey: key)
+			]
+		case .vtsi(let index):
+			return [
+				.menus: menuProgramChains(forKey: key),
+				.titles: titleProgramChains(forIndex: index)
+			]
+		}
 	}
 }
 
