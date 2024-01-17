62c6bb4506c0bbbdefb6e57c91db42f0d3700b04
Michael Roitzsch <reactorcontrol@icloud.com>
2024-01-17 19:47:53 +0100

    project: cleanup flake

--- Dependencies/flake.nix
+++ Dependencies/flake.nix
@@ -2,11 +2,10 @@
 	description = "build environment for dependencies";
 	outputs = { self, nixpkgs }: let
 		systems = [ "aarch64-darwin" "x86_64-darwin" ];
-		lib = import "${nixpkgs}/lib";
-		forAll = list: f: lib.genAttrs list f;
+		forAll = list: f: nixpkgs.lib.genAttrs list f;
 	in {
 		devShells = forAll systems (system: {
-			default = with import nixpkgs { inherit system; };
+			default = with nixpkgs.legacyPackages.${system};
 				mkShellNoCC {
 					packages = [
 						autoconf automake libtool pkg-config meson ninja cmake nasm
