8b141edb99116b0262d7e40f5b65678db0ffeca2
Michael Roitzsch <reactorcontrol@icloud.com>
2024-04-14 18:46:02 +0200

    importer: add DVDDataSource struct
    empty for now

---
+++ Importers/DVD/DataSource.swift
@@ -0,0 +1,7 @@
+import MovieArchiveModel
+
+
+/// A data source to read assets from a DVD.
+struct DVDDataSource: MediaDataSource {
+	// TODO: add info like start sector to read assets using a DVDReader
+}
